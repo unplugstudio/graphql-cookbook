67266fe9603b341be8b828e7f22f63b57e4d3791
Eduardo Rivas <e@jerivas.com>
2018-10-08 23:40:31 -0600

    Implement the Relay Node interface on Ingredient and CategoryNode.

--- recipes/schema/ingredients.py
+++ recipes/schema/ingredients.py
@@ -1,4 +1,5 @@
-import graphene
+from graphene import relay
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
 from ..models import Category, Ingredient
@@ -9,23 +10,18 @@ class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
         exclude_fields = ['ingredients']
+        interfaces = [relay.Node]
+        filter_fields = []
 
 
 class IngredientNode(DjangoObjectType):
 
     class Meta:
         model = Ingredient
+        interfaces = [relay.Node]
+        filter_fields = ['category__name']
 
 
 class IngredientQuery(object):
-    all_categories = graphene.List(CategoryNode)
-    all_ingredients = graphene.List(IngredientNode, category=graphene.String())
-
-    def resolve_all_categories(self, info, **kwargs):
-        return Category.objects.all()
-
-    def resolve_all_ingredients(self, info, **kwargs):
-        category = kwargs.get('category')
-        if category is None:
-            return Ingredient.objects.all()
-        return Ingredient.objects.filter(category__name=category)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
